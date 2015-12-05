99843642aef19d96060f0cdfe99c828240545e02
mehdi <mehdi@cherti.name>
2015-12-05 14:48:33 +0100

    support directories

--- store/app.py
+++ store/app.py
@@ -15,22 +15,34 @@ Url = namedtuple('Url', ['name', 'link'])
 
 @app.route('/')
 def index():
-    filenames = filter(file_pattern.search,
-                       os.listdir(config.dirname))
-    urls = map(lambda filename: Url(name=filename, link=filename),
-               filenames)
-    return render_template("index.html", urls=urls)
+    return get_path('/')
+
+
+@app.route('/<path:path>')
+def get_path(path):
+    path = os.path.join(config.dirname, path)
+    abs_path = config.dirname + "/" + path
+    if os.path.isdir(abs_path):
+        if not path.endswith("/"):
+            path += "/"
+        filenames = filter(file_pattern.search,
+                           os.listdir(abs_path))
+
+        def get_url(filename):
+            return Url(name=filename, link=path + filename)
+        urls = map(get_url, filenames)
+        return render_template("index.html", urls=urls)
+    else:
+        return get_filename(abs_path)
+
+
+def get_filename(filename):
+    if not os.path.exists(filename):
+        return "404 - {} not found".format(filename), 404
+    else:
+        return send_file(filename)
 
 
 @app.route('/stores')
 def stores():
     return jsonify(mirrors=get_mirrors())
-
-
-@app.route('/<filename>')
-def get_filename(filename):
-    filename = os.path.join(config.dirname, filename)
-    if not os.path.exists(filename):
-        return "404 - {} not found".format(filename), 404
-    else:
-        return send_file(filename)
